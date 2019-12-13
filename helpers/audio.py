Fixed bugs in TF spectrogram computation and added subsampling of loaded audio

helpers/audio.py
@@ -10,13 +10,15 @@ import os
 import decimal
 import math
 
-def decode_audio(fp, sample_rate=None):
+
+def decode_audio(fp, tgt_sample_rate=16000):
     audio_sf, audio_sr = sf.read(fp)
     if audio_sf.ndim > 1:
         print('warning: collapsing stereo into mono')
         audio_sf = audio_sf.mean(axis=-1)
-    if audio_sr != 16000:
-        print('warning: sampling frequency different than 16kHz ({})!'.format(audio_sr))
+    if audio_sr != tgt_sample_rate:
+        print('warning: sampling frequency different than {:,d} Hz ({:,d})! naively downsampling!'.format(tgt_sample_rate, audio_sr))
+        audio_sf = audio_sf[::int(np.ceil(audio_sr / tgt_sample_rate))]
     return audio_sf
 
 
@@ -48,25 +50,26 @@ def cache_noise_data(noise_paths, sample_rate=16000):
 
 
 def get_tf_spectrum(signal, sample_rate=16000, frame_size=0.025, frame_stride=0.01, num_fft=512):
-    assert sample_rate > 0 and frame_size > 0 and frame_stride > 0 and frame_stride < frame_size and num_fft > 0
+    assert sample_rate > 0 and frame_size > 0 and frame_stride > 0 and num_fft > 0
+    assert frame_stride < frame_size
 
-    signal.set_shape((None, None, 1))
-    frames = tf.contrib.signal.frame(signal, frame_length=int(frame_size*sample_rate), frame_step=int(frame_stride*sample_rate), pad_end=True, name='frames', axis=1)
-    hamming_window = tf.contrib.signal.hamming_window(int(frame_size*sample_rate), periodic=True)
-    frames = frames * tf.reshape(hamming_window, [1, 1, -1, 1])
-    frames = tf.pad(frames, [[0, 0], [0, 0], [0, int(num_fft) - int(frame_size*sample_rate)], [0, 0]])
-    frames = tf.transpose(frames, perm=[0, 1, 3, 2])
-    frames = tf.cast(frames, tf.complex64)
+    signal = tf.squeeze(signal, axis=-1)
 
-    magnitude_spectrum = tf.cast(tf.abs(tf.spectral.fft(frames, name='fft')), tf.float32)
-    magnitude_spectrum = tf.transpose(magnitude_spectrum, perm=[0, 3, 1, 2])
+    # Compute the spectrogram
+    magnitude_spectrum = tf.signal.stft(signal, int(frame_size*sample_rate), int(frame_stride*sample_rate), fft_length=num_fft)
+    magnitude_spectrum = tf.abs(magnitude_spectrum)
+    magnitude_spectrum = tf.transpose(magnitude_spectrum, perm=[0, 2, 1])
+    magnitude_spectrum = tf.expand_dims(magnitude_spectrum, 3)
 
-    mean_tensor, variance_tensor = tf.nn.moments(magnitude_spectrum, axes=[1])
+    # Normalize frames
+    agg_axis = 2
+    mean_tensor, variance_tensor = tf.nn.moments(magnitude_spectrum, axes=[agg_axis])
     std_dev_tensor = tf.math.sqrt(variance_tensor)
-    normalized_spectrum = (magnitude_spectrum - tf.expand_dims(mean_tensor, 1)) / tf.maximum(tf.expand_dims(std_dev_tensor, 1), 1e-8)
+    normalized_spectrum = (magnitude_spectrum - tf.expand_dims(mean_tensor, agg_axis)) / tf.maximum(tf.expand_dims(std_dev_tensor, agg_axis), 1e-12)
 
+    # Make sure the dimensions are as expected
     normalized_spectrum_shape = normalized_spectrum.get_shape().as_list()
-    assert normalized_spectrum_shape[1] == num_fft and normalized_spectrum_shape[3] == 1
+    assert normalized_spectrum_shape[1] == num_fft / 2 + 1 and normalized_spectrum_shape[3] == 1
 
     return normalized_spectrum
 
@@ -102,9 +105,9 @@ def play_n_rec(input_x, speaker=None, room=None, microphone=None, return_placeho
     room = room or tf.placeholder(tf.float32, [None, 1, 1], name='room')
     microphone = microphone or tf.placeholder(tf.float32, [None, 1, 1], name='microphone')
 
+    output = input_x
     output = tf.nn.conv1d(
-        # input_x,
-        tf.pad(input_x, [[0, 0], [0, tf.shape(speaker)[0]], [0, 0]], 'constant'),
+        tf.pad(output, [[0, 0], [0, tf.shape(speaker)[0]], [0, 0]], 'constant'),
         speaker, 1, padding="VALID")
 
     if noise_strength == 'random':
@@ -162,7 +165,7 @@ def framesig(sig, frame_len, frame_step, winfunc=lambda x: np.ones((x,)), stride
     return frames * win
 
 
-def normalize_frames(m,epsilon=1e-12):
+def normalize_frames(m, epsilon=1e-12):
     frames = []
     means = []
     stds = []
@@ -180,5 +183,6 @@ def get_fft_spectrum(signal, sample_rate, num_fft=512, frame_size=0.025, frame_s
     # get FFT spectrum
     frames = framesig(signal, frame_len=frame_size * sample_rate, frame_step=frame_stride * sample_rate, winfunc=np.hamming)
     fft = abs(np.fft.fft(frames, n=num_fft))
+    fft = fft[:, :(num_fft // 2 + 1)]
     fft_norm, fft_mean, fft_std = normalize_frames(fft.T)
     return fft_norm, fft_mean, fft_std

tests/combine_models.py
@@ -14,8 +14,10 @@ from models.verifier.vggvox import VggVox
 use_keras = True
 tgt_length = 410
 gan_model = ('male', 'female', 'neutral')[-1]
-latent_space = ('uniform', 'normal')[-1]
+latent_space = ('uniform', 'normal')[0]
 sampling = 16000
+refeed_target = True
+test_gradients = False
 
 # %% Load playback IRs
 
@@ -84,7 +86,7 @@ print('embedding  e: {}'.format(e.shape))
 if latent_space == 'normal':
     latent = np.random.normal(size=(1, 100))
 elif latent_space == 'uniform':
-    latent = np.random.uniform(size=(1, 100))
+    latent = np.random.uniform(size=(1, 100), low=-1, high=1)
 else:
     print('error: unknown latent space!')
     sys.exit(1)
@@ -108,7 +110,7 @@ print('output    : {} -> {:.30s}...'.format(out.shape, str(out.round(3)).replace
 # %% Define master voice gradients
 
 with graph.as_default():
-    h = tf.compat.v1.placeholder(tf.float32, (None, 512, tgt_length, 1))
+    h = tf.compat.v1.placeholder(tf.float32, (None, 257, tgt_length, 1))
     e2 = speaker_embedding(h)
     cos_distance = tf.keras.losses.CosineSimilarity(axis=1)
     sim = cos_distance(e, e2)
@@ -120,7 +122,6 @@ print('gradients  g: {}'.format(g.shape))
 # %% Test data along the way
 
 print('\n## Target sample:')
-# St = np.random.uniform(size=(1, 512, 410, 1)).astype(np.float32)
 xt = audio.read(os.path.join(root, 'data/voxceleb/test/id10273/5TWpQYtboq0/00001.wav'))
 St, _, _ = audio.get_fft_spectrum(xt.ravel(), sampling)
 print('tgt_speech xt: {} -> [{:.2f}, {:.2f}]'.format(xt.shape, xt.min(), xt.max()))
@@ -130,7 +131,7 @@ if St.shape[-1] > tgt_length:
     print('warning: clipping target speech to {} samples!'.format(tgt_length))
     St = St[:, :tgt_length]
 
-St = St.reshape((1, 512, -1, 1))
+St = St.reshape((1, 257, -1, 1))
 
 fd = {
         z: latent,
@@ -139,6 +140,9 @@ fd = {
         p['room']: room,
         p['speaker']: speaker,
 }
+
+if refeed_target:
+    fd[x] = xt[:65536].reshape((1, -1, 1))
 
 z_, x_, X_, S_, e_, et_ = sess.run([z, x, X, S, e, e2], feed_dict=fd)
 
@@ -168,9 +172,9 @@ axes[1,1].set_title('Playback of GAN sample [1st sec]')
 axes[1,0].plot(xt.ravel()[:sampling])
 axes[1,0].set_title('Target speech [1st sec]')
 
-axes[2,1].imshow(S_.reshape(512, -1), aspect='auto')
+axes[2,1].imshow(S_.reshape(257, -1)[:, :256], aspect='auto')
 axes[2,1].set_title('Spec GAN sample [{:.2f}, {:.2f}]'.format(S_.min(), S_.max()))
-axes[2,0].imshow(St.reshape(512, -1), aspect='auto')
+axes[2,0].imshow(St.reshape(257, -1)[:, :256], aspect='auto')
 axes[2,0].set_title('Target spec [{:.2f}, {:.2f}]'.format(St.min(), St.max()))
 
 axes[3,0].plot(e_.ravel(), et_.ravel(), 'o')
@@ -183,20 +187,22 @@ axes[3,1].legend(['GAN spect', 'Target spect'])
 
 plt.show()
 
-#%% Test gradients
+# %% Test gradients
 
-with graph.as_default():
+if test_gradients:
 
-    out_g = sess.run(g, feed_dict=fd)
+    with graph.as_default():
 
-print('\n## Gradients:')
-print('out grads   : {} -> {:.30s}...'.format(out_g.shape, str(out_g.round(3)).replace('\n', ' ') ))
+        out_g = sess.run(g, feed_dict=fd)
 
-# %% Print speaker verification and trainable variables
+    print('\n## Gradients:')
+    print('out grads   : {} -> {:.30s}...'.format(out_g.shape, str(out_g.round(3)).replace('\n', ' ') ))
 
-print('\n## SV Model Details:')
-print('Speaker embedding: {}'.format(speaker_embedding))
-print('Trainable variables:')
-with graph.as_default():
-    for v in tf.trainable_variables():
-        print(' ', v.name)
+    # %% Print speaker verification and trainable variables
+
+    print('\n## SV Model Details:')
+    print('Speaker embedding: {}'.format(speaker_embedding))
+    print('Trainable variables:')
+    with graph.as_default():
+        for v in tf.trainable_variables():
+            print(' ', v.name)