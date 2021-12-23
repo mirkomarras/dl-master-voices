deb824999d471fefa44f53e3b0afd1ee760534db
Paweł Korus <pkorus@gmail.com>
2021-12-23 16:27:08 -0500

    Added support for PESQ metric

--- models/mv/model.py
+++ models/mv/model.py
@@ -16,6 +16,8 @@ from models.verifier.model import Model
 from helpers.dataset import Dataset
 from helpers import plotting, audio
 
+from pesq import pesq
+
 from loguru import logger
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -223,7 +225,7 @@ class SiameseModel(object):
                 stats = json.load(f)
         else:
             # Collect stats from all seed voices
-            stats = {k: [] for k in 'max_dist,mean_abs,mse,mv_eer_results,mv_far1_results,sv_eer_results,sv_far1_results'.split(',')}
+            stats = {k: [] for k in 'max_dist,mean_abs,mse,mv_eer_results,mv_far1_results,sv_eer_results,sv_far1_results,pesq'.split(',')}
         
         for iter in range(n_seed_start, n_seed_voices): # For each audio sample to batch_optimize_by_path
             logger.info(f'Starting optimization {seed_voices[iter]}: {iter+1} of {n_seed_voices}')
@@ -268,6 +270,7 @@ class SiameseModel(object):
             stats['sv_eer_results'].append(performance['mv_eer_results'][0][gender].item())
             stats['sv_far1_results'].append(performance['mv_far1_results'][0][gender].item())
             stats['mse'].append(performance['mse'][-1])
+            stats['pesq'].append(performance['pesq'])
             stats['mean_abs'].append(performance['mean_abs'][-1])
             stats['max_dist'].append(performance['max_dist'][-1])
 
@@ -290,7 +293,7 @@ class SiameseModel(object):
 
         settings = settings or self.defaults()
 
-        metrics = ('mv_eer_results', 'mv_far1_results', 'mv_avg_similarity', 'mse', 'mean_abs','max_dist', 'snr')
+        metrics = ('mv_eer_results', 'mv_far1_results', 'mv_avg_similarity', 'mse', 'mean_abs','max_dist', 'snr', 'pesq')
         performance = {m: [] for m in metrics}
 
         remaining_attempts = settings.patience
@@ -410,35 +413,6 @@ class SiameseModel(object):
         np.save(os.path.join(self.dir_full, 'mv', suffix), mv_spec)
         sf.write(os.path.join(self.dir_full, 'mv', suffix + '.wav'), mv_wave, self.sample_rate)
 
-        # # We save the current audio associated to the master voice latent vector / spectrogram
-        # if self.gan is not None:
-        #     sp = self.gan.get_generator()(np.expand_dims(input_mv, axis=0))[-1].numpy()
-        #     sp = np.squeeze(sp)
-        # else:
-        #     sp = np.squeeze(audio.denormalize_frames(np.squeeze(input_mv), input_avg, input_std))
-        # sp = np.vstack((sp, np.zeros((1, sp.shape[1])), sp[:0:-1]))
-        # sp = sp.clip(0)
-        # sp_mv = sp
-
-        # We save the current master voice latent vector, if we are using a GAN-based procedure
-        # if self.gan is not None:
-        #     np.save(os.path.join(self.dir_full, 'mv_' + suffix), input_mv)
-
-        # We save the current audio associated to the seed voice latent vector / spectrogram
-        # sp = np.squeeze(self.gan.get_generator()(np.expand_dims(input_sv, axis=0))[-1].numpy()) if self.gan is not None else np.squeeze(audio.denormalize_frames(np.squeeze(input_sv), input_avg, input_std))
-        # sp = np.vstack((sp, np.zeros((1, sp.shape[1])), sp[:0:-1]))
-        # sp = sp.clip(0)
-
-        # # We save the current seed voice latent vector, if we are using a GAN-based procedure
-        # if self.gan is not None:
-        #     np.save(os.path.join(self.dir_full, 'sv_' + str(iter)), input_sv)
-
-        # # We save the unnormalized spectrogram of the seed voice        
-        # np.save(os.path.join(self.dir_full, 'sv_' + suffix), sp)
-
-        # inv_signal = audio.spectrum_to_signal(sp.T, int((sp.shape[1] + 1) / 100.0 * self.sample_rate), verbose=False)
-        # sf.write(os.path.join(self.dir_full, 'sv_' + suffix + '.wav'), inv_signal, self.sample_rate)
-
         # Plot a comparion between seed and attack spectrograms -------------------------------------------------------
         gender = self.params.mv_gender[0] # Gender selector: 'm' or 'f'
 
@@ -499,22 +473,12 @@ class SiameseModel(object):
             power_signal = np.mean(np.square(seed_wave))
             power_noise = np.mean(np.square(seed_wave - mv_wave))
         else:
-            len_diff = len(seed_wave) - len(mv_wave)
-            power_noise = 1e6
-            for dx in range(len_diff):
-                power_noise2 = np.mean(np.square(seed_wave[dx:len(mv_wave)+dx] - mv_wave))
-                if power_noise2 < power_noise:
-                    power_noise = power_noise2
-
-            seed_wave_inv = self.ensure_waveform(seed_spec, aux_signal=seed_wave)
-            if len(seed_wave_inv) == len(mv_wave):
-                power_signal = np.mean(np.square(seed_wave_inv))
-                power_noise = np.mean(np.square(seed_wave_inv - mv_wave))
-            else:
-                power_signal = 1
-                power_noise = 1
+            power_signal = 1
+            power_noise = 1
 
         performance_stats['snr'] = 10 * np.math.log10(power_signal / power_noise)
+        performance_stats['pesq'] = pesq(16000, seed_wave, mv_wave, 'wb')
+        logger.info(f'PESQ score: {performance_stats["pesq"]:.2f}')
 
         # We update and save the current impersonation rate history
         filename_stats = os.path.join(self.dir_full, f'opt_progress_{suffix}.npz')
