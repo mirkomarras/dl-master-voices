9286d393c00c42ccd2134c28fe85cf7dbaf4f323
Paweł Korus <pkorus@gmail.com>
2022-01-03 16:01:42 -0500

    Added results on transferability

--- helpers/results.py
+++ helpers/results.py
@@ -63,7 +63,7 @@ def cat_df(column_name, frames):
     return pd.concat(frames.values(), ignore_index=True)
 
 
-def progress(dirname, pattern=None, pad=True):
+def progress(dirname, pattern=None, pad=True, gender='f'):
     """Return optimization progress for a series of optimization runs
     """
     results = {}
@@ -86,7 +86,7 @@ def progress(dirname, pattern=None, pad=True):
 
         for pfile in glob.glob(os.path.join(dirname, subdir, 'opt_progress*.npz')):
             pdata = {x: y for x, y in np.load(pfile, allow_pickle=True).items()}
-            results[subdir].append([x['f'] for x in pdata['mv_far1_results']])
+            results[subdir].append([x[gender] for x in pdata['mv_far1_results']])
             
     # Pad results 
     if pad:
@@ -97,3 +97,54 @@ def progress(dirname, pattern=None, pad=True):
                     items.extend([items[-1]] * (max_len - len(items)))
     
     return results, labels
+
+
+def transferability(dirname, versions=(0,3), gender_index=1, play=False):
+    # data/results/transfer/vggvox_v000_pgd_wave_f/v000/mv/
+    # mv_test_population_interspeech_1000u_10s-resnet34_v000-avg-far1-0.npz
+    FN_PREFIX = 'mv_test_population_interspeech_1000u_10s'
+    FN_PREFIX = 'mv_test_population_libri_100u_10s'
+    arch = ('resnet50', 'thin_resnet', 'vggvox', 'xvector')
+    arch_short = ('R50', 'TR', 'V', 'X')
+    encoders = [f'{se}_v000' for se in arch]
+
+    df = pd.DataFrame()
+
+    for target_dir in sorted(os.listdir(dirname)):
+
+        new_row = {
+            # 'dirname': target_dir, 
+            'target': target_dir.split('_')[0]
+            }
+
+        for version in versions:
+
+            prefix = os.path.join(dirname, target_dir, f'v{version:03d}')
+
+            with open(os.path.join(prefix, 'params.txt')) as f:
+                params = f.read()
+                args = eval(eval(params))
+
+            for i, enc in enumerate(encoders):
+
+                # seed voices
+                filename = os.path.join(prefix, 'sv', f'{FN_PREFIX}-{enc}-avg-far1-{play:1d}.npz')
+                pdata = {x: y for x, y in np.load(filename, allow_pickle=True).items()}
+                ir_per_gender = pdata['results'].item()['gnds'].mean(0)
+
+                new_row[f'sv/{arch_short[i]}'] = 100 * ir_per_gender[gender_index] 
+
+                # master voices
+                filename = os.path.join(prefix, 'mv', f'{FN_PREFIX}-{enc}-avg-far1-{play:1d}.npz')
+                pdata = {x: y for x, y in np.load(filename, allow_pickle=True).items()}
+                ir_per_gender = pdata['results'].item()['gnds'].mean(0)
+
+                new_row[f'mv({args.step_size_override})/{arch_short[i]}'] = 100 * ir_per_gender[gender_index] 
+                
+        df = df.append(new_row, ignore_index=True)
+        # df.loc[target_dir.split('_')[0]] = new_row
+
+    df = df.set_index('target')
+    df.columns = pd.MultiIndex.from_tuples([c.split('/') for c in df.columns])
+
+    return df
