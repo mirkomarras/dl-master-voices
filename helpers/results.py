c866a47b941f5911754761ca6a5f38284aaa1807
Paweł Korus <pkorus@gmail.com>
2022-01-03 08:24:36 -0500

    Updates in results & plotting

--- helpers/results.py
+++ helpers/results.py
@@ -7,15 +7,19 @@ import pandas as pd
 
 from argparse import Namespace
 
-def results_df(dirname):
+def results_df(dirname, pattern=None):
     """Return DataFrame that summarizes a series of optimization runs
     """
-    df = pd.DataFrame(columns=('run_id', 'epsilon', 'steps', 'step_size_override', 'clip_av', 'far1-sv', 'far1-mv', 'nes_n', 'nes_sigma', 'mse', 'max_dist'))
+    df = pd.DataFrame(columns=('run_id', 'epsilon', 'steps', 'step_size_override', 'clip_av', 'far1-sv', 'far1-mv', 
+                               'nes_n', 'nes_sigma', 'mse', 'max_dist', 'pesq'))
     
     if not os.path.isdir(dirname):
         raise IOError('Directory does not exist...')
     
     for subdir in sorted(os.listdir(dirname)):
+        
+        if pattern is not None and not re.match(pattern, subdir):
+            continue
     
         with open(os.path.join(dirname, subdir, 'params.txt')) as f:
             params = f.read()
@@ -28,8 +32,9 @@ def results_df(dirname):
             'run_id' : subdir,
             'epsilon' : args.epsilon,
             'steps': args.n_steps,
-            'step_size_override' : args.step_size_override,
+            'step_size' : args.step_size_override,
             'mse': np.mean(train_stats['mse']),
+            'pesq': np.mean(train_stats['pesq']),
             'max_dist': np.mean(train_stats['max_dist']),
             'nes_n' : args.nes_n,
             'nes_sigma' : args.nes_sigma,
@@ -40,6 +45,12 @@ def results_df(dirname):
     
     # Drop columns with all NaNs
     df = df.dropna(how='all', axis=1)
+    
+    # Drop columns with all identical entries
+    for col in df.columns:
+        if len(df[col].unique()) == 1:
+            df = df.drop(columns=col)
+    
     return df
 
 
@@ -52,7 +63,7 @@ def cat_df(column_name, frames):
     return pd.concat(frames.values(), ignore_index=True)
 
 
-def progress(dirname, pattern=None):
+def progress(dirname, pattern=None, pad=True):
     """Return optimization progress for a series of optimization runs
     """
     results = {}
@@ -76,5 +87,13 @@ def progress(dirname, pattern=None):
         for pfile in glob.glob(os.path.join(dirname, subdir, 'opt_progress*.npz')):
             pdata = {x: y for x, y in np.load(pfile, allow_pickle=True).items()}
             results[subdir].append([x['f'] for x in pdata['mv_far1_results']])
+            
+    # Pad results 
+    if pad:
+        for k, v in results.items():            
+            max_len = max(len(x) for x in v)
+            for items in v:
+                if len(items) < max_len:
+                    items.extend([items[-1]] * (max_len - len(items)))
     
     return results, labels
--- summarize.py
+++ summarize.py
@@ -25,24 +25,29 @@ import numpy as np
 
 from helpers import results as hr
 
-df = hr.results_df('data/results/results_plain_pgd/vggvox_v000_pgd_wave_f')
+df = hr.results_df('data/results/play_normed/vggvox_v000_pgd_wave_f/')
+# df = df[df['nes_sigma'] == 0.001]
 
 print(df.to_string())
 
 # %%
 
-c1 = 'step_size_override'
-c2 = 'clip_av'
-
+c1 = 'nes_n'
+c2 = 'step_size_override'
+    
 n1 = len(df[c1].unique())
 n2 = len(df[c2].unique())
 
-plt.imshow(df['far1-mv'].values.reshape((n2, n1)))
-plt.xticks(np.arange(0,n1), df[c1].unique())
-plt.yticks(np.arange(0,n2), df[c2].unique())
-plt.colorbar()
-plt.xlabel(c1)
-plt.ylabel(c2)
+# plt.imshow(df['far1-mv'].values.reshape((n2, n1)).T)
+sns.heatmap(df['far1-mv'].values.reshape((n2, n1)).T, annot=True)
+plt.gca().set_xticks(np.arange(0,n1) + 0.5)
+plt.gca().set_xticklabels(df[c1].unique())
+plt.gca().set_yticks(np.arange(0,n2) + 0.5)
+plt.gca().set_yticklabels(df[c2].unique())
+# plt.gca().colorbar()
+plt.gca().set_xlabel(c1)
+plt.gca().set_ylabel(c2)
+plt.gca().set_title(f'NES sigma 0.001')
 
 # %%
 
