52e5a80004a37ef01fc8d3bff248d76eb3ac29bd
Paweł Korus <pkorus@gmail.com>
2022-01-05 12:40:31 -0500

    Updated results - more plots and tables

--- helpers/results.py
+++ helpers/results.py
@@ -99,6 +99,58 @@ def progress(dirname, pattern=None, pad=True, gender='f'):
     return results, labels
 
 
+def scatter(dirname, measurement='training', play=False, gender_index=1):
+    """Return DataFrame that summarizes a series of optimization runs
+    """
+    df = pd.DataFrame(columns=('run_id', 'epsilon', 'steps', 'step_size_override', 'clip_av', 'far1-sv', 'far1-mv', 
+                               'nes_n', 'nes_sigma', 'mse', 'max_dist', 'pesq'))
+    
+    if not os.path.isdir(dirname):
+        raise IOError('Directory does not exist...')
+        
+    with open(os.path.join(dirname, 'params.txt')) as f:
+        params = f.read()
+        args = eval(eval(params))
+    
+    with open(os.path.join(dirname, 'stats.json')) as f:
+        train_stats = json.load(f)
+    
+    # df = df.append({
+    #     'run_id' : subdir,
+    #     'epsilon' : args.epsilon,
+    #     'steps': args.n_steps,
+    #     'step_size' : args.step_size_override,
+    #     'mse': np.mean(train_stats['mse']),
+    #     'pesq': np.mean(train_stats['pesq']),
+    #     'max_dist': np.mean(train_stats['max_dist']),
+    #     'nes_n' : args.nes_n,
+    #     'nes_sigma' : args.nes_sigma,
+    #     'clip_av' : args.clip_av,
+    #     'far1-sv': np.mean(),
+    #     'far1-mv': np.mean()
+    #     }, ignore_index=True)
+
+    if measurement == 'training':
+        return train_stats['sv_far1_results'], train_stats['mv_far1_results'], np.mean(train_stats['pesq'])
+
+    else:
+        pattern = os.path.join(dirname, 'mv', f'*-{measurement}-{play:1d}.npz')
+        filename = glob.glob(pattern)[0]
+        pdata = {x: y for x, y in np.load(filename, allow_pickle=True).items()}
+        ir_mv = pdata['results'].item()['gnds'][:, gender_index]
+        # pdata['results'].item()['imps'].mean(1)
+
+        # print(np.mean(ir_mv), 
+
+        pattern = os.path.join(dirname, 'sv', f'*-{measurement}-{play:1d}.npz')
+        filename = glob.glob(pattern)[0]
+        pdata = {x: y for x, y in np.load(filename, allow_pickle=True).items()}
+        # ir_sv = pdata['results'].item()['imps'].mean(1)
+        ir_sv = pdata['results'].item()['gnds'][:, gender_index]
+
+        return ir_sv, ir_mv, np.mean(train_stats['pesq'])
+
+
 def transferability(dirname, versions=(0,3), gender_index=1, play=False):
     # data/results/transfer/vggvox_v000_pgd_wave_f/v000/mv/
     # mv_test_population_interspeech_1000u_10s-resnet34_v000-avg-far1-0.npz
